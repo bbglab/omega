8b2528360b82827f51a5fe947caa1b062fecb173
Federica Brando <federica.brando@irbbarcelona.org>
2025-11-27 12:20:10 +0100

    lint: globals file

--- omega/src/globals.py
+++ omega/src/globals.py
@@ -21,29 +21,33 @@ FORMAT = '%(asctime)s - %(color)s%(levelname)-7s%(color_stop)s | %(name)s - %(co
 #  Logging
 # =========
 
+
 def setup_logging_decorator(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
-        log_dir = os.path.join('.','log')
+        log_dir = os.path.join('.', 'log')
         command_name = click.get_current_context().command.name
 
         if command_name == 'run':
-            cohort = click.get_current_context().params["cohort"]
+            cohort = click.get_current_context().params['cohort']
             fname = f'{cohort if not "None" else command_name}_{DATE}.log'
-        else: 
-            fname = f"{command_name}_{DATE}.log"
+        else:
+            fname = f'{command_name}_{DATE}.log'
 
         os.makedirs(log_dir, exist_ok=True)
-        
-        level = logging.DEBUG # if click.get_current_context().params['verbose'] else logging.INFO
 
-        formatter = daiquiri.formatter.ColorFormatter(fmt=FORMAT)
-        
-        daiquiri.setup(level=level, outputs=(
-            daiquiri.output.Stream(formatter=formatter), 
-            daiquiri.output.File(filename=os.path.join(log_dir, fname), formatter=formatter)
-        ))
-        
+        level = logging.DEBUG  # if click.get_current_context().params['verbose'] else logging.INFO
+
+        formatter = ColorFormatter(fmt=FORMAT)
+
+        daiquiri.setup(
+            level=level,
+            outputs=(
+                daiquiri.output.Stream(formatter=formatter),
+                daiquiri.output.File(filename=os.path.join(log_dir, fname), formatter=formatter),
+            ),
+        )
+
         return func(*args, **kwargs)
 
     return wrapper
@@ -80,12 +84,13 @@ def clean_directory(path: str, loc: str) -> None:
         path (str): Path to the directory to be cleaned.
     """
 
-    if loc == "d":
-
-        clean_files = f"rm -rf {os.path.join(path, '*.csv')} {os.path.join(path, '*.json')} {os.path.join(path, '.*.txt')}"
-        clean_pae = ["rm", "-rf", os.path.join(path, "pae")]
-        clean_pdb = ["rm", "-rf", os.path.join(path, "pdb_structures")]
-        clean_pcmaps = ["rm", "-rf", os.path.join(path, "prob_cmaps")]
+    if loc == 'd':
+        clean_files = (
+            f'rm -rf {os.path.join(path, "*.csv")} {os.path.join(path, "*.json")} {os.path.join(path, ".*.txt")}'
+        )
+        clean_pae = ['rm', '-rf', os.path.join(path, 'pae')]
+        clean_pdb = ['rm', '-rf', os.path.join(path, 'pdb_structures')]
+        clean_pcmaps = ['rm', '-rf', os.path.join(path, 'prob_cmaps')]
 
         logger.debug(clean_files)
         subprocess.run(clean_files, shell=True)
@@ -99,7 +104,7 @@ def clean_directory(path: str, loc: str) -> None:
         logger.debug(' '.join(clean_pcmaps))
         subprocess.run(clean_pcmaps)
 
-    elif loc == "r":
+    elif loc == 'r':
         # TODO: implement cleaning function for output
         pass
 
@@ -113,34 +118,34 @@ def clean_dir(path: str, loc: str = 'd') -> None:
     """
 
     if os.listdir(path) != ['log']:
-        logger.warning(f"Directory {path} already exists and is not empty.")
+        logger.warning(f'Directory {path} already exists and is not empty.')
 
-        overwrite = "y" if click.get_current_context().params['yes'] else input("Clean existing directory? (y/n): ")
-        while overwrite.lower() not in ["y", "yes", "n", "no"]:
-            print("Please choose yes or no")
-            overwrite = input("Clean existing directory? (y/n): ")
+        overwrite = 'y' if click.get_current_context().params['yes'] else input('Clean existing directory? (y/n): ')
+        while overwrite.lower() not in ['y', 'yes', 'n', 'no']:
+            print('Please choose yes or no')
+            overwrite = input('Clean existing directory? (y/n): ')
 
-        if overwrite.lower() in ["y", "yes"]:
+        if overwrite.lower() in ['y', 'yes']:
             clean_directory(path, loc)
-            logger.info(f"Dataset files in {path} have been removed.")
+            logger.info(f'Dataset files in {path} have been removed.')
         else:
-            logger.warning(f"Dataset files in {path} have not been removed.")
+            logger.warning(f'Dataset files in {path} have not been removed.')
     else:
         pass
-    
-    
+
+
 def clean_temp_files(path: str, keep_pdb_files: bool) -> None:
     """
-    Clean temp files from dir after completing building the datasets. 
+    Clean temp files from dir after completing building the datasets.
 
     Args:
         path (str): Path to build directory to be cleaned.
     """
-    
+
     if not keep_pdb_files:
-        clean_pdb = ["rm", "-rf", os.path.join(path, "pdb_structures")]
+        clean_pdb = ['rm', '-rf', os.path.join(path, 'pdb_structures')]
         logger.debug(' '.join(clean_pdb))
         subprocess.run(clean_pdb)
-    clean_pae = ["rm", "-rf", os.path.join(path, "pae", "*.json")]
+    clean_pae = ['rm', '-rf', os.path.join(path, 'pae', '*.json')]
     logger.debug(' '.join(clean_pae))
     subprocess.run(clean_pae)
